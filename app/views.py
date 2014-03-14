b786a7e102306349f081ae430a8d59a4be3f2f7e
sant0sh <san00718@gmail.com>
2014-03-15 01:10:09 +0530

    Template inheritance added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
+    user={'nickname':'Santosh'}
+    posts=[
+            {
+                'author':{'nickname':'John'},
+                'body': 'The text1 is working fine'
+            },
+            {
+                'author':{'nickname':'Susan'},
+                'body':'The text2 is working'
+            }
+          ]
+    return render_template('index.html',
+            title="Welcome",
+            user=user,
+            posts=posts
+            )
+
