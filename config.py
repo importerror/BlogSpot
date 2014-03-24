cbd12d2643e43033ca525ca9b6d84446798b0227
sant0sh <san00718@gmail.com>
2014-03-24 15:56:43 +0530

    OpenID providers added

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ KEY=base64.b64encode(os.urandom(32))
 
 CSRF_ENABLE = True
 SECRET_KEY=KEY
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
